ce7943df59f7e9518e9c44325370fc9fa42bd59e
Titus Wormer <tituswormer@gmail.com>
2021-03-12 16:57:19 +0100

    Add JSDoc based types

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ var map = {
   '␦': 26 /* SUB */
 }
 
+/**
+ * Replace control pictures with the actual control codes.
+ *
+ * Supports U+2400 SYMBOL FOR NULL (␀) through U+2426 SYMBOL FOR SUBSTITUTE
+ * FORM TWO (`␦`).
+ *
+ * @param {string} [value] Value with control pictures
+ * @returns {string} Value but with actual control codes
+ */
 export function controlPictures(value) {
   return String(value == null ? '' : value).replace(
     /[\u2400-\u2426]/g,
@@ -16,6 +25,9 @@ export function controlPictures(value) {
   )
 }
 
+/**
+ * @param {string} d
+ */
 function replacer(d) {
   return String.fromCharCode(
     own.call(map, d) ? map[d] : d.charCodeAt(0) - 9216 /* `␀` */
