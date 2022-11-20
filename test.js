746f734220b5c5a783cfacd0be7cc1ed5e282a66
Titus Wormer <tituswormer@gmail.com>
2022-11-20 11:41:56 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,23 +1,22 @@
-import test from 'tape'
+import assert from 'node:assert/strict'
+import test from 'node:test'
 import {controlPictures} from './index.js'
 
-test('control-pictures', function (t) {
-  t.equal(controlPictures(), '', 'should not fail without a value')
-  t.equal(controlPictures('␉␊␋␌␍'), '\t\n\v\f\r', 'should work')
-  t.equal(controlPictures('␀'), '\0', 'should support `␀`')
-  t.equal(controlPictures('a␡b'), 'a\u007Fb', 'should support `␡`')
+test('control-pictures', function () {
+  assert.equal(controlPictures(), '', 'should not fail without a value')
+  assert.equal(controlPictures('␉␊␋␌␍'), '\t\n\v\f\r', 'should work')
+  assert.equal(controlPictures('␀'), '\0', 'should support `␀`')
+  assert.equal(controlPictures('a␡b'), 'a\u007Fb', 'should support `␡`')
 
-  t.equal(
+  assert.equal(
     controlPictures('␢␣'),
     controlPictures('␠␠'),
     'should support alt forms of spaces'
   )
-  t.equal(controlPictures('␤'), '\n', 'should use line feeds for `␤`')
-  t.equal(
+  assert.equal(controlPictures('␤'), '\n', 'should use line feeds for `␤`')
+  assert.equal(
     controlPictures('␥␦'),
     controlPictures('␡␚'),
     'should support alt forms of DEL, SUB'
   )
-
-  t.end()
 })
